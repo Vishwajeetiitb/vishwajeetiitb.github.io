669c1f5c984728ac6f9f25d870579d5359d9182b
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-05-19 19:01:34 +0530

    projects updated

---
+++ js/tab.js
@@ -0,0 +1,21 @@
+const buttonElement = document.querySelectorAll('.tablinks');
+const tabContent = document.querySelectorAll(".tabcontent");
+
+tabContent[0].style.display = "block";
+
+buttonElement.forEach(function (i) {
+    i.addEventListener('click', function (event) {
+
+        for (let x = 0; x < buttonElement.length; x++) {
+            if (event.target.id == buttonElement[x].id) {
+                buttonElement[x].className = buttonElement[x].className.replace(" active", "");
+                tabContent[x].style.display = "block";
+                event.currentTarget.className += " active";
+            } else {
+                tabContent[x].style.display = "none";
+                buttonElement[x].className = buttonElement[x].className.replace(" active", "");
+            }
+        }
+        
+    });
+});
